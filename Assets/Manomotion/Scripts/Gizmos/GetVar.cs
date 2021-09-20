5ede6b27c6074441976238bd4b8e26c721fee2cd
christiancasin <89009263+christiancasin@users.noreply.github.com>
2021-09-20 22:44:15 +0800

    Initial commit

---
+++ Assets/Manomotion/Scripts/Gizmos/GetVar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetVar : MonoBehaviour
+{
+    public Text textDisplay;
+    public Text summary;
+
+    void Update()
+    {
+        int receivedCount = TriggerGizmo.countingGesture;
+        textDisplay.text = "" + receivedCount;
+        if (receivedCount >= 10 && receivedCount <= 12)
+        {
+            summary.text = "Nice Job!";
+        }else if (receivedCount <= 9)
+        {
+            summary.text = "Please Follow the Gesture Properly!";
+        }else if (receivedCount >= 13)
+        {
+            summary.text = "Please Slow Down!";
+        }
+    }
+}
